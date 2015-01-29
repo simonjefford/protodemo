895ec66b421fa6eb0b5f374b81812ef644bd8083
Simon Jefford <simon.jefford@fourth.com>
2015-01-29 18:04:43 +0000

    server done

--- go/server.go
+++ go/server.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
 
 	people "./fourth_people"
 	"code.google.com/p/goprotobuf/proto"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	test := &people.Employee{}
-	test.Name = proto.String("Simon")
-	test.Age = proto.Int32(37)
-	test.NiNumber = proto.String("JJ817203D")
-	d, _ := proto.Marshal(test)
+	startHttpServer()
+}
 
-	newTest := &people.Employee{}
+func sendError(w http.ResponseWriter, v interface{}) {
+	w.WriteHeader(http.StatusBadRequest)
+	fmt.Fprint(w, v)
+	log.Println(v)
+}
 
-	proto.Unmarshal(d, newTest)
+func employeeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.ContentLength <= 0 {
+		sendError(w, "no bytes received")
+		return
+	}
 
-	log.Println(newTest)
+	b := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(b)
 
-	log.Println(d)
+	if err != nil && err != io.EOF {
+		sendError(w, err)
+		return
+	}
+
+	e := &people.Employee{}
+	err = proto.Unmarshal(b, e)
+
+	if err != nil {
+		sendError(w, err)
+		return
+	}
+
+	log.Println("Parsed output:", e)
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func startHttpServer() {
+	r := mux.NewRouter()
+	r.HandleFunc("/ReceiveEmployee", employeeHandler)
+	log.Println("starting")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
